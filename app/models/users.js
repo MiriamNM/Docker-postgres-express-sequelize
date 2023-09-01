dab3ea5e76e028b2db95d92c520eba442e9697c7
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-09-01 13:42:44 -0600

    Who to create db witn docker, postgres, sequelize, express

---
+++ app/models/users.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+const db = require("../util/database");
+
+const User = db.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
